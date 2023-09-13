b4d813f1daed65834357dc2edc2551ff2154f1fd
seongwop <psw921@gmail.com>
2023-09-14 01:16:29 +0900

    feat : add a master step for ãpartitioning

--- spring-batch-practice/src/main/java/com/example/pointmanagement/job/reservation/ExecutePointReservationStepConfiguration.java
+++ spring-batch-practice/src/main/java/com/example/pointmanagement/job/reservation/ExecutePointReservationStepConfiguration.java
@@ -1,38 +1,70 @@
 package com.example.pointmanagement.job.reservation;
 
+import com.example.pointmanagement.job.reader.ReverseJpaPagingItemReader;
+import com.example.pointmanagement.job.reader.ReverseJpaPagingItemReaderBuilder;
 import com.example.pointmanagement.point.Point;
 import com.example.pointmanagement.point.PointRepository;
 import com.example.pointmanagement.point.reservation.PointReservation;
 import com.example.pointmanagement.point.reservation.PointReservationRepository;
 import com.example.pointmanagement.point.wallet.PointWallet;
 import com.example.pointmanagement.point.wallet.PointWalletRepository;
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.partition.support.TaskExecutorPartitionHandler;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.JpaPagingItemReader;
-import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.util.Pair;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.time.LocalDate;
-import java.util.Map;
 
 @Configuration
 public class ExecutePointReservationStepConfiguration {
+
+    @Bean
+    @JobScope
+    public Step executePointReservationMasterStep(
+            JobRepository jobRepository,
+            TaskExecutorPartitionHandler partitionHandler,
+            PointReservationRepository pointReservationRepository,
+            @Value("#{T(java.time.LocalDate).parse(jobParameters[today])}")
+            LocalDate today
+    ) {
+        return new StepBuilder("executePointReservationMasterStep", jobRepository)
+                .partitioner(
+                        "executePointReservationStep",
+                        new ExecutePointReservationStepPartitioner(pointReservationRepository, today)
+                )
+                .partitionHandler(partitionHandler)
+                .build();
+    }
+
+    @Bean
+    public TaskExecutorPartitionHandler partitionHandler( // 로컬에서 쓰레드로 실행
+            Step executePointReservationStep,
+            TaskExecutor taskExecutor
+    ) {
+        TaskExecutorPartitionHandler partitionHandler = new TaskExecutorPartitionHandler();
+        partitionHandler.setStep(executePointReservationStep);
+        partitionHandler.setGridSize(8);
+        partitionHandler.setTaskExecutor(taskExecutor);
+        return partitionHandler;
+    }
+
     @Bean
     @JobScope
     public Step executePointReservationStep(
             JobRepository jobRepository,
             PlatformTransactionManager platformTransactionManager,
-            JpaPagingItemReader<PointReservation> executePointReservationItemReader,
+            ReverseJpaPagingItemReader<PointReservation> executePointReservationItemReader,
             ItemProcessor<PointReservation, Pair<PointReservation, Point>> executePointReservationItemProcessor,
             ItemWriter<Pair<PointReservation, Point>> executePointReservationItemWriter
 
@@ -46,20 +78,38 @@ public class ExecutePointReservationStepConfiguration {
                 .build();
     }
 
+//    @Bean
+//    @StepScope
+//    public JpaPagingItemReader<PointReservation> executePointReservationItemReader(
+//            EntityManagerFactory entityManagerFactory,
+//            @Value("#{T(java.time.LocalDate).parse(jobParameters[today])}")
+//            LocalDate today
+//
+//    ) {
+//        return new JpaPagingItemReaderBuilder<PointReservation>()
+//                .name("executePointReservationItemReader")
+//                .entityManagerFactory(entityManagerFactory)
+//                .queryString("select pr from PointReservation pr where pr.earnedDate = :today and pr.executed = false")
+//                .parameterValues(Map.of("today", today))
+//                .pageSize(1000)
+//                .build();
+//    }
+
     @Bean
     @StepScope
-    public JpaPagingItemReader<PointReservation> executePointReservationItemReader(
-            EntityManagerFactory entityManagerFactory,
-            @Value("#{T(java.time.LocalDate).parse(jobParameters[today])}")
-            LocalDate today
-
+    public ReverseJpaPagingItemReader<PointReservation> executePointReservationItemReader(
+            PointReservationRepository pointReservationRepository,
+            @Value("#{T(java.time.LocalDate).parse(jobParameters[today])}") LocalDate today,
+            @Value("#{stepExecutionContext[minId]}") Long minId,
+            @Value("#{stepExecutionContext[maxId]}") Long maxId
     ) {
-        return new JpaPagingItemReaderBuilder<PointReservation>()
-                .name("executePointReservationItemReader")
-                .entityManagerFactory(entityManagerFactory)
-                .queryString("select pr from PointReservation pr where pr.earnedDate = :today and pr.executed = false")
-                .parameterValues(Map.of("today", today))
+        return new ReverseJpaPagingItemReaderBuilder<PointReservation>()
+                .name("messageExpireSoonPointItemReader")
+                .query(
+                        pageable -> pointReservationRepository.findPointReservationToExecute(today, minId, maxId, pageable)
+                )
                 .pageSize(1000)
+                .sort(Sort.by(Sort.Direction.ASC, "id"))
                 .build();
     }
 
--- spring-batch-practice/src/main/java/com/example/pointmanagement/point/reservation/PointReservationCustomRepository.java
+++ spring-batch-practice/src/main/java/com/example/pointmanagement/point/reservation/PointReservationCustomRepository.java
@@ -1,10 +1,13 @@
 package com.example.pointmanagement.point.reservation;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.time.LocalDate;
 
 public interface PointReservationCustomRepository {
+    Page<PointReservation> findPointReservationToExecute(LocalDate today, Long minId, Long maxId, Pageable pageable);
 
     Long findMinId(LocalDate today);
-
     Long findMaxId(LocalDate today);
 }
